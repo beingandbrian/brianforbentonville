c0b0ad4e57bf158a1531a72aeac0716365c88c97
nebojsa <nebojsa@teams.bet>
2023-02-12 00:11:11 +0100

    compose

--- config/settings/base.py
+++ config/settings/base.py
@@ -131,12 +131,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
+STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 MEDIA_URL = '/media/'
