129618a1e14761a5d06192af212f5f3f17aa7148
nebojsa <nebojsa@teams.bet>
2023-02-20 20:47:17 +0100

    tmplte

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,9 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/staticfiles/'
-
-STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 MEDIA_URL = '/media/'
